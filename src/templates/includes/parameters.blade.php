832739eef9a44996d09055b1080623ec4afcb129
Mateusz Pęczkowski <peczis@gmail.com>
2017-05-30 13:04:30 +0200

    fixes

--- src/templates/includes/parameters.blade.php
+++ src/templates/includes/parameters.blade.php
@@ -1,4 +1,4 @@
-                             <li id="{li-id}">
+                             <li>
                                 <div class="parameter-name">{param-name}</div>
                                 <div class="parameter-type">{param-type}</div>
                                 <div class="parameter-desc">{param-desc}</div>
